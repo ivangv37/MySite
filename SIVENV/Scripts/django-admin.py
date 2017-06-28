64c175eb90acdba15c6e2dafb984531a828acb61
GeekTale <geektalechannel@gmail.com>
2017-06-28 19:21:10 +0200

    .idea/workspace.xml change

---
+++ SIVENV/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Academia\Desktop\Seguridad Informática\Python\source\django\SIVENV\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
